25113310e91c5d6628101db43b9773b248e0f491
godfathercorleone994-wq <godfathercorleone994@gmail.com>
2025-10-20 12:26:22 -0300

    Create init.py

---
+++ models/init.py
@@ -0,0 +1,13 @@
+from .product import Product, ProductStatus, PriceHistory
+from .extraction_result import ExtractionResult, ExtractionStatus
+from .notification import Notification, NotificationType
+
+__all__ = [
+    "Product",
+    "ProductStatus",
+    "PriceHistory",
+    "ExtractionResult",
+    "ExtractionStatus",
+    "Notification",
+    "NotificationType"
+]
